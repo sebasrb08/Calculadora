61d14dae384e2325c4aff73541fd35ce555bd3ab
sebastian <sebasr1268@gmail.com>
2023-02-28 12:56:43 -0500

    añadiendo calculadora

---
+++ main.js
@@ -0,0 +1,45 @@
+num4=document.getElementById('num1')
+num3=document.getElementById('num2')
+
+function mostrar(){
+    document.getElementById('display').innerText=resultado
+   
+}
+function operador(operando){
+    switch(operando){
+        case '+':
+            resultado =parseInt(num1.value)+parseInt(num2.value)
+            simbolo.innerText='+'
+    }
+}
+function operador2(operando2){
+    switch(operando2){
+        case '-':
+            resultado=parseInt(num1.value)-parseInt(num2.value)
+            simbolo.innerText='-'
+
+    }
+}
+function operador3(operando3){
+    switch(operando3){
+        case '*':
+            resultado=parseInt(num1.value)*parseInt(num2.value)
+            simbolo.innerText='*'
+
+    }
+
+}
+function operador4(operando4){
+    switch(operando4){
+        case '/':
+            resultado=parseInt(num1.value)/parseInt(num2.value)
+            simbolo.innerText='/'
+
+        }
+}
+function borrar(){
+    num1.value=''
+    num2.value=''
+    simbolo.innerText=''
+    display.innerText=''
+}
