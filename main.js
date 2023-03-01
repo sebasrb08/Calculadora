0079a7a251ac84a85b91804920d3066df996eb5f
sebastian <sebasr1268@gmail.com>
2023-03-01 14:00:39 -0500

    añadiendo codigo

--- main.js
+++ main.js
@@ -1,38 +1,63 @@
 
 num=[]
-num2=[]
-num3=[]
-num4=[]
+
+
+for(let i=0;i<=9;i++){
+    document.getElementById('btn_'+ i).addEventListener('click',(boton)=>mostrar2(boton.target));
+
+}
+function mostrar2(tecla){
+num1=document.getElementById('display').innerText=
+  num1= document.getElementById('display').innerText+tecla.value
+
+}
+addEventListener('keypress',teclas=>{
+    if(!isNaN(teclas.key)){
+        mostrar3(teclas.key)
+    }
+    if(teclas.key=='+'){
+        operador(teclas.key)
+    }if(teclas.key=='-'){
+        operador2(teclas.key)
+    }if(teclas.key=='*'){
+        operador3(teclas.key)
+    }if(teclas.key=='/'){
+        operador4(teclas.key)
+    }if(teclas.key=='Enter'){
+        mostrar(teclas.key)
+    }
+    
+})
+function mostrar3(teclas){
+    num1=document.getElementById('display').innerText=
+    num1=document.getElementById('display').innerText+teclas
+}
+
 function mostrar(){
 
     if(simbolo.value=='+'){
-        num[1]=num1.value
-        resultado =parseInt(num[0])+parseInt(num[1])
+        resultado =parseInt(num[0])+parseInt(num1)
     }
     else if(simbolo.value=='-'){
-        num2[1]=num1.value
-        resultado =parseInt(num2[0])- parseInt(num2[1])
- 
+        resultado =parseInt(num[1])- parseInt(num1)
     }
     else if(simbolo.value=='*'){
-        num3[1]=num1.value
-        resultado =parseInt(num3[0])*parseInt(num3[1])
+    
+        resultado =parseInt(num[2])*parseInt(num1)
  
     }else if(simbolo.value=='/'){
-        num4[1]=num1.value
-        resultado =parseInt(num4[0])/parseInt(num4[1])
+        resultado =parseInt(num[3])/parseInt(num1)
  
     }
+    console.log(resultado)
     document.getElementById('display').innerText=resultado
 
 }
 function operador(operando){
     switch(operando){
         case '+':
-            num[0]=num1.value
-            if(num[0]==num1.value){
-                num1.value=''
-            }
+            num[0]=num1
+            display.innerText=''
             simbolo.value='+'
             
     }
@@ -40,10 +65,8 @@ function operador(operando){
 function operador2(operando2){
     switch(operando2){
         case '-':
-            num2[0]=num1.value
-            if(num2[0]==num1.value){
-                num1.value=''
-            }
+            num[1]=num1
+            display.innerText=''
             simbolo.value='-'
             
 
@@ -52,31 +75,21 @@ function operador2(operando2){
 function operador3(operando3){
     switch(operando3){
         case '*':
-            num3[0]=num1.value
-            if(num3[0]==num1.value){
-                num1.value=''
-            }
-            
+            num[2]=num1
+            display.innerText=''
             simbolo.value='*'
-
-
     }
 
 }
 function operador4(operando4){
     switch(operando4){
         case '/':
-            num4[0]=num1.value
-            if(num4[0]==num1.value){
-                num1.value=''
-            }           
+            num[3]=num1
+            display.innerText=''
             simbolo.value='/'
-
-
         }
 }
 function borrar(){
-    num1.value=''
-    num2.value=''
+   
     display.innerText=''
 }
